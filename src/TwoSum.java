52254a70f6137ab0e8002248c298dba7ddf51d45
Zhanibek <zhttaubaldiev@edu.hse.ru>
2019-10-09 23:19:04 +0300

    TwoSum fixed

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -5,15 +5,16 @@ public class TwoSum {
     class Solution {
         public int[] twoSum(int[] nums, int target) {
             Map<Integer, Integer> map = new HashMap<>();
-            for(int i = 0; i < nums.length; i++)
+            for (int i = 0; i < nums.length; i++)
                 map.put(nums[i], i);
 
-            for(int i = 0; i < nums.length; i++){
+            for (int i = 0; i < nums.length; i++) {
                 int complement = target - nums[i];
-                if(map.containsKey(complement) && map.get(complement) != i){
-                    return new int[] {i, map.get(complement)};
+                if (map.containsKey(complement) && map.get(complement) != i) {
+                    return new int[]{i, map.get(complement)};
                 }
             }
             throw new IllegalArgumentException("No two sum solution");
         }
     }
+}
