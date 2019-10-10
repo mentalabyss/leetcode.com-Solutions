ba10cbd1165366c6b088444fd20aaa6f1e04e823
Zhanibek <zhttaubaldiev@edu.hse.ru>
2019-10-10 20:29:06 +0300

    reverse integer added

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -14,6 +14,7 @@ public class ValidParentheses {
         }
 
         public boolean isValid(String s) {
+
             Stack<Character> stack = new Stack<>();
 
             for(int i = 0; i < s.length(); i++){
